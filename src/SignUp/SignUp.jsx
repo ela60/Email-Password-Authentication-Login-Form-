2ce78bd31ae1d7223a47b7b7f2e273765f5bf10c
Ambia-Ela <ambiaela7275@gmail.com>
2024-11-10 20:13:36 +0600

    All form releted complete

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,41 +1,161 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../Firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
+  const [success, setSuccess] = useState(false);
+  const [error, setErrorMessage] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+      const password = e.target.password.value;
+      const terms = e.target.terms.checked;
+      const name = e.target.name.value;
+      const photoURL = e.target.photo.value;
+    console.log(email, password);
+
+    //   reset error and status
+      setErrorMessage("");
+      setSuccess(false);
+
+      if (!terms) {
+          setErrorMessage('please accept our terms and conditions');
+          return;
+      }  
+
+
+    if (password.length < 6) {
+      setErrorMessage("Password should be at least 6 characters or longer");
+      return;
+    }
+
+    const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "At least one uppercase,one lowercase,one number,one special character"
+      );
+      return;
+    }
+
+    //   create user email with password
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+          setSuccess(true);
+          
+          //   send verification email address
+          sendEmailVerification(auth.currentUser)
+              .then(() => {
+                  console.log('verification email send');
+              });
+          //   update profile name and photo url
+          const profile = {
+              displayName: name,
+              photoURL: photo
+          }
+          updateProfile(auth.currentUser, profile)
+              .then(() => {
+                  console.log('user profile updated');
+              })
+              .catch(error => console.log('user profile updated'));
+      })
+      .catch((error) => {
+        console.log("ERROR", error.message);
+        setErrorMessage(error.message);
+        setSuccess(false);
+      });
+  };
+
+  return (
+    <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-4xl font-bold mx-auto my-4">Sign up now!</h1>
+      <form onSubmit={handleSignUp} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            className="input input-bordered"
+            required
+                  />
+                  </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo URL</span>
+          </label>
+          <input
+            type="text"
+            name="photo"
+            placeholder="photo URL"
+            className="input input-bordered"
+            required
+                  />
+                  </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control relative">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-xs absolute right-4 top-12"
+          >
+            {showPassword ? <FaEye /> : <FaEyeSlash />}
+          </button>
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+          <div className="form-control">
+            <label className="cursor-pointer justify-start label">
+              <input
+                type="checkbox"name="terms"
+                className="checkbox checkbox-warning"
+              />
+              <span className="label-text ml-2">Accept our terms and condition</span>
+            </label>
           </div>
         </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Sign Up</button>
+        </div>
+      </form>
+      {error && <p className="text-3xl text-red-800">{error}</p>}
+          {success && <p className="text-green-500">Sign up successful</p>}
+          <p className='m-2'>Already have an account? Please <Link
+                    >Login</Link></p>
       </div>
-    );
+      
+  );
 };
 
 export default SignUp;
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,10 +1,88 @@
-import React from 'react';
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
+import { auth } from '../Firebase.init';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [success, setSuccess] = useState(false);
+    const [loginError, setLoginError] = useState('');
+    const emailRef = useRef();
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        // login user
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+
+                if (!result.user.emailVerified) {
+                    setLoginError('Please verify your email address')
+                }
+                else {
+                    setSuccess(true);
+                }
+
+                setSuccess(true);
+            })
+            .catch(error => {
+            console.log('ERROr',error.message);
+        })
+    }
+
+    const handleForgetPassword = () => {
+        console.log('get me email address');
+        const email = emailRef.current.value;
+        if (!email) {
+            console.log('Please provide a valid email address');
+        }
+        else {
+            sendPasswordResetEmail(auth, email)
+                .then(() => {
+                alert('Password Reset email sent,please check your email ')
+            })
+        }
+
+    }
+
     return (
-        <div>
-            <h2 className="text-3xl">login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form  onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                            <input type="email" ref={emailRef }name='email'placeholder="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                <label onClick={handleForgetPassword} className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+                    </form>
+                    <p className='m-2'>Already have an account? Please <Link
+                    >Sign Up</Link></p>
+          </div>
         </div>
+      </div>
     );
 };
 
